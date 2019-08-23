3f42ae7138ee595445a121cf625d1dd5d67d710a
Karen V <karenvo@usc.edu>
2019-08-23 03:42:26 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,28 +9,43 @@ class Pokemon
     @db = db
   end
 
-  def self.save(name:, type:, db:)
-      sql = <<-SQL
-        INSERT INTO pokemon (name, type)
-        VALUES (?, ?)
-      SQL
-      db.execute(sql, [name, type])
-  end
+  # def self.save(name:, type:, db:)
+  #     sql = <<-SQL
+  #       INSERT INTO pokemon (name, type)
+  #       VALUES (?, ?)
+  #     SQL
+  #     db.execute(sql, [name, type])
+  # end
 
   def update
     sql = "UPDATE pokemon SET name = ?, type = ?, WHERE id = ?"
     db.execute(sql, self.name, self.type, self.id)
   end
 
-  def self.find(id, db)
+  # def self.find(id, db)
+  #   sql = <<-SQL
+  #     SELECT *
+  #     FROM pokemon
+  #     WHERE id = ?
+  #     LIMIT 1
+  #   SQL
+  #   pokemon = db.execute(sql,[id]).flatten
+  #   Pokemon.new(id: id, name: pokemon[1], type: pokemon[2], db: db)
+  # end
+  def self.save(name, type, db)
     sql = <<-SQL
-      SELECT *
-      FROM pokemon
-      WHERE id = ?
-      LIMIT 1
+      INSERT INTO pokemon (name, type) VALUES (?, ?);
     SQL
-    pokemon = db.execute(sql,[id]).flatten
-    Pokemon.new(id: id, name: pokemon[1], type: pokemon[2], db: db)
+    
+    db.execute(sql, [name, type])
+    
   end
 
+  def self.find(id, db)
+    sql = <<-SQL
+      SELECT * FROM pokemon WHERE id = (?);
+    SQL
+    pokemon = db.execute(sql, [id]).flatten
+    Pokemon.new(id, pokemon[1], pokemon[2], pokemon[3], db )
+  end
 end
