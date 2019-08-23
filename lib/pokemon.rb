22a8387f4c6016a497b6aaf1ecdc68dabb216141
Karen V <karenvo@usc.edu>
2019-08-23 03:45:46 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,15 +8,7 @@ class Pokemon
     @type = type
     @db = db
   end
-
-  # def self.save(name:, type:, db:)
-  #     sql = <<-SQL
-  #       INSERT INTO pokemon (name, type)
-  #       VALUES (?, ?)
-  #     SQL
-  #     db.execute(sql, [name, type])
-  # end
-
+  
   def update
     sql = "UPDATE pokemon SET name = ?, type = ?, WHERE id = ?"
     db.execute(sql, self.name, self.type, self.id)
@@ -46,6 +38,6 @@ class Pokemon
       SELECT * FROM pokemon WHERE id = (?);
     SQL
     pokemon = db.execute(sql, [id]).flatten
-    Pokemon.new(id, pokemon[1], pokemon[2], pokemon[3], db )
+    Pokemon.new(id: id, pokemon[1], pokemon[2], pokemon[3], db )
   end
 end
